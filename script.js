0da051d700cfe9f24a5f13780fa60beeb82e2111
johnanwar <johnanwar30@gmail.com>
2019-06-05 16:54:46 +0300

    Ff

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ class AudiCcontoller{
     this.bgSOund = new Audio("Assets/Audio/creepy.mp3");
     this.flibSound = new Audio('Assets/Audio/flip.wav');
     this.gameoverSound = new Audio('Assets/Audio/gameOver.wav');
+    this.matchSound = new Audio('assets/Audio/match.wav')
     this.victorySound = new Audio('Assets/Audio/victory.wav');
     this.bgSOund.loop = true;
     this.bgSOund.volume =.5;
@@ -16,11 +17,15 @@ class AudiCcontoller{
 
   stopMusic(){
     this.bgSOund.pause();
-    this.bgSOund.currentTime() = 0;
+    // this.bgSOund.currentTime() = 0;
   };
 
   flib() {
     this.flibSound.play();
+  };
+
+  matchMusic() {
+    this.matchSound.play();
   };
 
   victory() {
@@ -49,40 +54,135 @@ class GameCotroller {
   };
 
   startGame (){
-    this.cardCheck = null;
+    this.cardToCheck = null;
     this.matchCards = [];
     this.timeRemaning = this.totalTime
     this.pusy= true;    // if i can click it 
     this.cardCheck = null;
-    this.audiCcontoller.startMusic();
-    this.suffleCards(this.cardsArray);
+
+    // to restrt the game 
+    setTimeout(() => {
+      this.suffleCards(this.cardsArray);
+      this.audiCcontoller.startMusic();
+      this.pusy = false;
+      this.countDown = this.StartCountDown();
+    },500)
+
+    // this.hideCards() {
+    //   this.time.textContent = this.timeRemaning;
+    //   this.flibs.textContent = this.allFlibs;
+    // };
+  };
+  hideCards(){
+    this.cardsArray.forEach( (card)=>{
+      card.classList.remove('visible');
+      card.classList.remove('dance');
+    });
+
   };
 
-  suffleCards(cardsArray) {
-    for (let i = cardsArray.length -1; i > 0; i--) {
-      let random = Math.floor(Math.random() * (i + 1))
-      cardsArray[random].style.order = i;
-      cardsArray[i].style.order = random;
-    }
-
-  };
+  // click on cards
   flibCard(card){
-    if (this.canFlibGamme(card) === false) { 
+    if (this.canFlibGamme(card) === true) { 
       this.audiCcontoller.flib(); 
       this.allFlibs ++;
       this.flibs.textContent = this.allFlibs;
-      card.classList.add("visible")
-      console.log(this.allFlibs)
+      card.classList.add("visible");
+
+      if (this.cardToCheck){
+        this.checkForCarMatch(card)
+      }else{
+        this.cardToCheck = card ;
+      };
     };
   };
- 
 
-  canFlibGamme (card){
-   // this.!pusy ..... !this.matchCards include (card) ...... card !===this.cardCheck
-   return true;;
+
+  /// match cards
+  checkForCarMatch(card) {
+    if (this.getCardType(card) === this.getCardType(this.cardToCheck) ){
+      //match
+      this.cardMatch(card, this.cardToCheck);
+    }else{
+      //didnt match
+      this.cardMisMatch(card , this.cardToCheck)
+      this.cardToCheck = null;
+    };
+
+  };
+      //match
+  cardMatch(card1 , card2){
+    this.matchCards.push(card1);
+    this.matchCards.push(card2);
+    card1.classList.add('dance');
+    card2.classList.add('dance');
+    this.audiCcontoller.matchMusic();
+    if (this.matchCards.length === this.cardsArray.length)
+      this.victory();
+
+      };
+
+    //didnt match
+  cardMisMatch(card1, card2){
+    this.busy = true;
+    setTimeout(() => {
+      card1.classList.remove('visible');
+      card2.classList.remove('visible');
+      this.busy = false;
+    }, 1000)
+
   };
 
-}
+
+  // timer
+  StartCountDown (){
+     return setInterval( ()=> {
+       this.timeRemaning-- ;
+       this.time.textContent = this.timeRemaning;
+       if(this.timeRemaning === 0){
+         this.gameover()
+       }
+
+    },1000)
+  };
+    // game Over
+  gameover(){
+    this.audiCcontoller.gameOver();
+    this.hideCards();
+    clearInterval(this.countDown);
+    document.querySelector('.overlay3').style.display ="block"
+    };
+
+ //  victory
+  victory() {
+    this.audiCcontoller.victory();
+    this.hideCards();
+    clearInterval(this.countDown);
+    document.querySelector('.overlay2').style.display = "block"
+  };
+    // flib game
+    canFlibGamme(card){
+    // this.!pusy ..... !this.matchCards include (card) ...... card !===this.cardCheck
+    return true;;
+  };
+
+  //Random shufle cards
+  suffleCards(cardsArray) {
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+      let random = Math.floor(Math.random() * (i + 1))
+      cardsArray[random].style.order = i;
+      cardsArray[i].style.order = random;
+    };
+};
+
+
+  // getCardType
+  getCardType(card) {
+    //return card.getElemntsByClassName(".middle-ghost")[0].src;
+    return card.querySelectorAll('.middle-ghost')[0].src;
+  }
+
+};
 
 
 
@@ -93,7 +193,7 @@ var ready = function(){
 
     cards = document.querySelectorAll(".all-card");
     overlay = document.querySelectorAll(".overlay");
-    game = new GameCotroller(100 , cards) ;
+    game = new GameCotroller(60 , cards) ;
 
     // overlay function
       overlay.forEach ((over) => over.addEventListener('click', function(){
